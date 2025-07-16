b2d1af7b8139e8bb353d0bafa3e31c71b6bf8ef9
Krish-747 <krishith.v@gmail.com>
2025-07-17 00:17:41 +0530

    Now able to update road height and noisiness of road easily, and performed optimisations so that output is as intended for any amount of time

--- Passive Suspension.py
+++ Passive Suspension.py
@@ -4,21 +4,25 @@ from scipy.interpolate import make_interp_spline
 import matplotlib.pyplot as plt
 
 # * Constants
-mass = 5.0              # kg
+mass = 5.0             # kg
 spring_constant = 50.0  # N/m
 damping_constant = 50.0 # Ns/m
 g = 10.0                # m/s²
-spring_length = 10.0    # m
+spring_length = 20.0    # m
+
+# * Road Profile
+time_intervals = 100
+height_range = (-10, 10)
 
 # * Time 
 total_time_ms = 5000
 time_ms = np.linspace(0, total_time_ms, 1000)
 time_s = time_ms / 1000.0
-dt = time_s[1] - time_s[0]
+dt = 0.01
 
 # * Road
-road_x = np.linspace(0, total_time_ms, 10)
-road_y = [0] + [random.uniform(-2.5, 2.5) for _ in range(9)]
+road_x = np.linspace(0, total_time_ms, time_intervals)
+road_y = [0] + [random.uniform(height_range[0], height_range[1]) for _ in range(time_intervals-1)]
 road_spline = make_interp_spline(road_x, road_y)
 road_y = road_spline(time_ms)
 
@@ -53,10 +57,11 @@ plt.plot(time_ms, positions, label="Passive Suspension", color='orange')
 plt.axhline(initial_y, color='gray', linestyle=':', label="Height")
 
 plt.xlabel("Time (ms)")
-plt.xlim(0, 5000)
+plt.xlim(0, total_time_ms)
 
 plt.ylabel("Vertical Position (m)")
-plt.ylim(-4, 15)
+plt.ylim(height_range[0]-5, height_range[1]+initial_y)
+plt.yticks(np.arange(height_range[0], height_range[1]+initial_y, step=10))
 
 plt.title("Passive Suspension")
 plt.legend()
